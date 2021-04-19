724ff48f0e1c7a23d1d6bf3072cefe3727874e8f
simei-yeh <simei.yeh@gmail.com>
2021-04-19 16:07:09 -0700

    Updated dev environment and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,25 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
       },
     ],
   },

9539643000f6c54d53be137364512899943b311e
simei-yeh <simei.yeh@gmail.com>
2021-04-19 16:14:40 -0700

    Updated port

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require( 'express' );
 
 const app = express();
 
+const port = 3000;
+
 app.get( '/', ( req, res ) => {
   res.send({ greeting: 'Hello world!' });
 });
 
-app.listen( process.env.PORT );
+app.listen( port );

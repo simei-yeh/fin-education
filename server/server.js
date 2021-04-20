d851cdaeba0ba142f6abb5915696d33d5c103fdf
simei-yeh <simei.yeh@gmail.com>
2021-04-19 19:51:04 -0700

    Made server more modular

--- server/server.js
+++ server/server.js
@@ -1,11 +1,5 @@
-const express = require( 'express' );
-
-const app = express();
-
 const port = 3000;
 
-app.get( '/', ( req, res ) => {
-  res.send({ greeting: 'Hello world!' });
-});
+const app = require('./app.js')
 
 app.listen( port );
